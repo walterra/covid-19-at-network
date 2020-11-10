ae123f5037a94ec43c0efc651c94bdc1e828e16f
Walter Rafelsberger <walter@elastic.co>
2020-11-10 23:03:37 +0100

    click events

---
+++ docs/descriptions.js
@@ -0,0 +1,13 @@
+var desc = {};
+
+desc.fa = `
+"Kein Beleg fuer Maskenpflicht" — 21.07.2020, [Tiroler Tageszeitung](https://www.tt.com/artikel/30742756/experten-und-unternehmer-einig-kein-beleg-fuer-maskenpflicht)
+`;
+
+desc.ms = `
+Gesundheitswissenschaftler und Public-Health-Experte
+
+*"Trotz vorgezogener Virensaison (SARS-CoV-2 Welle und Grippewelle) wird es zu keiner Überforderung der Versorgung kommen."*<br />09.11.2020, [Public Health Graz](https://www.facebook.com/GrazPublicHealth/posts/5216750491683956)
+
+*"Eines ist sicher, zu einer Überforderung der Krankenversorgung wird es mit hundertprozentiger Sicherheit nicht kommen."*<br />22.08.2020, [orf.at](https://orf.at/stories/3178426/)
+`;
